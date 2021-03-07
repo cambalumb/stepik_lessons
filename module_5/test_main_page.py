5229689f719a167ef3859188c6cdda047ffaf3c7
cambalumb <cambalumb7@gmail.com>
2021-03-07 18:34:13 +0300

    the first object model

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,9 +1,8 @@
-from selenium import webdriver
+from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
-
 class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        browser.get(link)
-        login_link = browser.find_element_by_css_selector("#login_link")
-        login_link.click()
+        page = MainPage(browser,link)
+        page.Open()
+        page.go_to_login_page()
